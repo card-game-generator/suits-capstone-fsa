5c9e0e96119a865d5d701f788a7973b11f28c0dc
Jack Lye <catncradle@gmail.com>
2018-06-26 09:14:30 -0400

    parent form connected to child forms

--- src/components/CreateGame/PhaseForm/ParentForm.js
+++ src/components/CreateGame/PhaseForm/ParentForm.js
@@ -9,6 +9,7 @@ export default class FormContainer extends Component {
       formIdx: 1,
       players: 0,
       cards: 0,
+      turn: [],
     };
     this.handleState = this.handleState.bind(this);
   }
@@ -19,13 +20,26 @@ export default class FormContainer extends Component {
   }
 
   render() {
+    console.log(this.state);
     let idx = this.state.formIdx;
     return (
       <div>
         {idx === 1 ? <StartingRules handleSubmit={this.handleState} /> : null}
-        {/* {idx === 2 ? <PhaseForm /> : null} */}
-        <PhaseForm />
+        {idx === 2 ? <PhaseForm handleSubmit={this.handleState} /> : null}
+
         <div>players and cards populate here, maybe another subComponent</div>
+        <div>
+          just here to show local state of parent Number of players {this.state.players} Number of
+          cards for each player {this.state.cards}
+        </div>
+        {this.state.turn.map(phase => {
+          return (
+            <div key={phase}>
+              just a div showing phase {phase.source} {phase.sourceAction} {phase.target}
+              {phase.targetAction}
+            </div>
+          );
+        })}
       </div>
     );
   }
