d2610d01023cba87294a20e1ad827fc3b7d1daad
Jack Lye <catncradle@gmail.com>
2018-06-28 15:36:02 -0400

    drilling props from board to field to card

--- src/components/Board/Card.js
+++ src/components/Board/Card.js
@@ -1,32 +1,21 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// // import BoardContext from './BoardContext';
+//This component renders out our card and receives props from the player/field
+export default class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-// //This component renders out our card and receives props from the player
-// export default class Card extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
+  render() {
+    let player = this.props.player || null;
+    let field = this.props.field || null;
 
-//   render() {
-//     // let player = this.props.player;
-
-//     return (
-//       <div>
-//         {/*  TODO rewrite file once consumer is implemented
-//         {  player.isCurrentPlayer
-//             ? this.props.card
-//             : new Array(this.props.card.length).fill({ suit: 'hidden', value: 'card' });
-//           return (
-//             <div className="card-container">
-//               <div name="card" className={`${card.suit} ${card.value}`}>
-//                 {card.suit}
-//                 {card.value}
-//               </div>
-//             </div>
-//           );} */}
-//         hi
-//       </div>
-//     );
-//   }
-// }
+    return (
+      <div>
+        TODO rewrite file once consumer is implemented
+        {player.isCurrentPlayer || field ? <div>card</div> : null}
+        hiiiiiiiiiiiiisdjfkdjslkfjldsjkl
+      </div>
+    );
+  }
+}
