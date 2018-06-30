bd0367279ea123404db25f096a0391760be93b28
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-06-30 13:22:49 -0400

    add(winCheck): add player with least cards condition to winCheckconditions

--- src/utils/Game/CurrentGame.js
+++ src/utils/Game/CurrentGame.js
@@ -50,8 +50,9 @@ export function winCheck(currPhase, state) {
 
   function winningConditions(condition) {
     let highestScore = 0,
-      highestCardCount = 0;
-    let winningPlayer = {};
+      highestCardCount = 0,
+      lowestCardCount = 52,
+      winningPlayer = {};
     players.forEach(player => {
       switch (condition) {
         case 'Player with highest score':
@@ -59,12 +60,22 @@ export function winCheck(currPhase, state) {
             highestScore = player.score;
             winningPlayer = player;
           }
+          // if two players both have the highest score then THERE IS NO WINNER!!!!!!
+          if (player.score === highestScore) winningPlayer = {};
           break;
         case 'Player with most cards':
           if (player.hand.length > highestCardCount) {
             highestCardCount = player.hand.length;
             winningPlayer = player;
           }
+          if (player.hand.length === highestCardCount) winningPlayer = {};
+          break;
+        case 'Player with least cards':
+          if (player.hand.length < lowestCardCount) {
+            lowestCardCount = player.hand.length;
+            winningPlayer = player;
+          }
+          if (player.hand.length === lowestCardCount) winningPlayer = {};
           break;
       }
     });
